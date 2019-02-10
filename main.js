3fcae7a3b0c772fbbc84d27ea9d05b9a26f00445
nadavatar <nadavatar64@gmail.com>
2019-02-10 21:58:36 +0200

    asdf

---
+++ main.js
@@ -0,0 +1,15 @@
+let computerScore = 0;
+let userScore = 0;
+const limit = 22;
+let isUserPlaying = true;
+
+userScore += Math.floor((Math.random()*13) + 1); 
+
+document.getElementById("userContainer").innerHTML += userScore;
+
+while (isUserPlaying && userScore < limit) {
+    
+}
+    
+        
+    
