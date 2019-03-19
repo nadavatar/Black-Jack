ff5ab4cb74e56c90eecde88c873a3218bb06ec61
nadavatar <nadavatar64@gmail.com>
2019-03-19 14:13:28 +0200

    changed the cards numbers

--- blackJack.py
+++ blackJack.py
@@ -7,16 +7,16 @@ isComputerPlaying = True
 isUserLost = False
 isComputerLost = False
 
-userScore += randint(1, 13)
+userScore += randint(1, 14)
 print("Your score is: " + str(userScore))
 
 choice = raw_input("If you want another card enter y, otherwise enter n:\n")
 if choice == "n":
     isUserPlaying = False
 
-while(userScore < 22 and isUserPlaying):
+while (userScore < 22 and isUserPlaying):
 
-    userScore += randint(1, 13)
+    userScore += randint(1, 14)
     if userScore > 21:
         print("You have: " + str(userScore) + " You Lost!")
         isUserPlaying = False
@@ -24,22 +24,24 @@ while(userScore < 22 and isUserPlaying):
 
     else:
         print("Your score is: " + str(userScore))
-        choice = raw_input("If you want another card enter y, otherwise enter n:\n")
+        choice = raw_input(
+            "If you want another card enter y, otherwise enter n:\n")
         if choice == "n":
             isUserPlaying = False
 
 if isUserLost != True:
 
-    computerScore += randint(1, 13)
+    computerScore += randint(1, 14)
     if computerScore > userScore:
         print("The computer score is: " + str(computerScore))
         print("You have lost...")
         isUserLost = True
-    
-    while(isComputerPlaying and computerScore < 21 and computerScore < userScore):
 
-        computerScore += randint(1, 13)
-        
+    while (isComputerPlaying and computerScore < 21
+           and computerScore < userScore):
+
+        computerScore += randint(1, 14)
+
         if computerScore < 21 and computerScore < userScore:
             print("The computer score is: " + str(computerScore))
 
